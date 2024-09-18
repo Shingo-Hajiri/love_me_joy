5e358a87305c3d6fa338a49ed283a918bcf57343
羽尻慎吾 <hajirishingo@hajirishingonoMacBook-Air.local>
2024-09-18 23:35:27 +0900

    回答結果画面に正答を表示

--- app/controllers/loves_controller.rb
+++ app/controllers/loves_controller.rb
@@ -17,26 +17,30 @@ class LovesController < ApplicationController
   def set_params
     @answers = params[:answers]
     @result = []
-    @result << @answers[:q16]
+    n = 0
+    (1..16).each do |i|
+      answer = @answers[:"q#{i}"] || '未回答'
+      @result << answer
+    end
   end
 
   def scoring(answers)
     score = 0
-    score += 5 if answers[:q1] == 'yes'
-    score += 5 if answers[:q2] == 'yes'
-    score += 5 if answers[:q3] == 'yes'
-    score += 5 if answers[:q4] == 'yes'
-    score += 5 if answers[:q5] == 'yes'
-    score += 7 if answers[:q6] == 'yes'
-    score += 7 if answers[:q7] == 'yes'
-    score += 7 if answers[:q8] == 'yes'
-    score += 7 if answers[:q9] == 'yes'
-    score += 7 if answers[:q10] == 'yes'
-    score += 8 if answers[:q11] == 'yes'
-    score += 8 if answers[:q12] == 'yes'
-    score += 8 if answers[:q13] == 'yes'
-    score += 8 if answers[:q14] == 'yes'
-    score += 8 if answers[:q15] == 'yes'
+    score += 5 if answers[:q1] == '⚪︎'
+    score += 5 if answers[:q2] == '⚪︎'
+    score += 5 if answers[:q3] == '⚪︎'
+    score += 5 if answers[:q4] == '⚪︎'
+    score += 5 if answers[:q5] == '⚪︎'
+    score += 7 if answers[:q6] == '⚪︎'
+    score += 7 if answers[:q7] == '⚪︎'
+    score += 7 if answers[:q8] == '⚪︎'
+    score += 7 if answers[:q9] == '⚪︎'
+    score += 7 if answers[:q10] == '⚪︎'
+    score += 8 if answers[:q11] == '⚪︎'
+    score += 8 if answers[:q12] == '⚪︎'
+    score += 8 if answers[:q13] == '⚪︎'
+    score += 8 if answers[:q14] == '⚪︎'
+    score += 8 if answers[:q15] == '⚪︎'
     score
   end
 end
